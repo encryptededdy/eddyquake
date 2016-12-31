30b8fad2445833e3a41125a70a8f278f6a2abfcf
encryptededdy <pcman2000@gmail.com>
2016-12-31 19:57:30 +1300

    Server doesn't actually take values over 8... oops

--- src/zhang/nz/Main.java
+++ src/zhang/nz/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner reader = new Scanner(System.in);
-        System.out.print("Enter minimum MMI (Modified Mercalli Intensity Scale), 1 - 10: ");
+        System.out.print("Enter minimum MMI (Modified Mercalli Intensity Scale), 1 - 8: ");
         int mmi = reader.nextInt();
         GeoNet.showQuakes(mmi);
         System.out.print("Enter Quake ID to show more information: ");
